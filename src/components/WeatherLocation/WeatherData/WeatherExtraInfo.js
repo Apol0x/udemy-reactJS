6a532c6acc5acc553d4c70ef7c2a390458d36cc7
apol0x <madejapolo@gmail.com>
2019-12-29 20:53:26 +0100

    MOD: reprogramming style classes and propTypes

--- src/components/WeatherLocation/WeatherData/WeatherExtraInfo.js
+++ src/components/WeatherLocation/WeatherData/WeatherExtraInfo.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import './style.css';
 
 const WeatherExtraInfo = ({ humidity, wind }) => (
-    <div className="wExtInfCont">
+    <div className="weatherExtraInfoCont">
         <span className="extraInfoText">{`Humedad: ${humidity}%`}</span>
         <span className="extraInfoText">{`Viento: ${wind}`}</span>
     </div>
--- src/components/WeatherLocation/WeatherData/WeatherTemperature.js
+++ src/components/WeatherLocation/WeatherData/WeatherTemperature.js
@@ -2,24 +2,25 @@ import React from "react";
 import WeatherIcons from "react-weathericons";
 import PropTypes from "prop-types";
 import "./style.css";
+import IconsConstanst from '../../../constans/weather';
 
-const icons = {
+/* const icons = {
   cloud: "cloud",
   cloudy: "cloudy",
   sun: "day-sunny",
   rain: "rain",
   snow: "snow",
   windy: "windy"
-};
+}; */
 const getWeatherIcons = weatherState => {
-  const icon = icons[weatherState];
+  const icon = IconsConstanst[weatherState];
   const sizeIcon = "4x";
 
-  if (icon) return <WeatherIcons className="wicon" name={icon} size="2x" />;
-  else return <WeatherIcons className="wicon" name="day-sunny" size="2x" />;
+  if (icon) return <WeatherIcons className="wicon" name={icon} size={sizeIcon} />;
+  else return <WeatherIcons className="wicon" name="day-sunny" size={sizeIcon} />;
 };
 const WeatherTemperature = ({ temperature, weatherState }) => (
-  <div className="wTempCont">
+  <div className="weatherTemperatureCont">
     {getWeatherIcons(weatherState)}
     <span className="temperature">{`${temperature}`}</span>
     <span className="temperatureType">{`ºC`}</span>
