eced2b391ce7eb064e24d5af6c38d77d1df151df
apol0x <madejapolo@gmail.com>
2020-01-18 17:46:00 +0100

    MOD: WeatherLocation become a class

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,30 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
 import './style.css';
 import {
-    /* CLOUD,
-    CLOUDY,
-    SUN,
-    RAIN, */
-    SNOW,
-    /* WINDY, */
-  } from '../../constans/weather';
+  /* CLOUD,
+  CLOUDY,
+  SUN,
+  RAIN, */
+  SNOW,
+  /* WINDY, */
+} from '../../constans/weather';
 
-  const dataWeatherData = {
-    temperature: 5,
-    weatherState: SNOW,
-    humidity: 10,
-    wind: "10 m/s",
-  };
+const dataWeatherData = {
+  temperature: 5,
+  weatherState: SNOW,
+  humidity: 10,
+  wind: "10 m/s",
+};
 
 //arrow function es una funcion que se declara de forma flecha
 // propia de ES6
-const WeatherLocation = () => (
-    <div className="weatherLocationCont"> 
-        <Location city={"Madrid"}/>
-        <WeatherData data={dataWeatherData}/>
-    </div>
-);
+class WeatherLocation extends Component {
+  render() {
+    return (
+      <div className="weatherLocationCont">
+        <Location city={"Madrid"} />
+        <WeatherData data={dataWeatherData} />
+      </div>
+    );
+  }
+};
 
 export default WeatherLocation;
