661def743db704b5e91c0e818a26bbd131be3135
apol0x <madejapolo@gmail.com>
2020-01-24 01:57:30 +0100

    MOD: using api response and updating real datas. :ok_hand:

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -6,19 +6,19 @@ import CONSTANT from '../../config.js';
 
 import './style.css';
 import {
-  /* CLOUD,
-  CLOUDY,
+  CLOUDS,
   SUN,
-  RAIN, */
+  RAIN,
   SNOW,
   CLOUDY,
-  /* WINDY, */
+  WINDY
 } from '../../constans/weather';
+import weatherIcon from '../../constans/weather';
 
 const location = "Seville,spa";
 const api_key = CONSTANT.API_WEATHER_KEY;
 const url_base_weather = CONSTANT.URL_BASE;
-const api_call = `${url_base_weather}?q=${location}&appid=${api_key}`;
+const api_call = `${url_base_weather}?q=${location}&units=metric&appid=${api_key}`;
 
 
 const weatherData = {
@@ -27,16 +27,7 @@ const weatherData = {
   humidity: 10,
   wind: "10 m/s",
 };
-
-const data2 = {
-  temperature: 27,
-  weatherState: SNOW,
-  humidity: 20,
-  wind: "20 m/s",
-};
-
-//arrow function es una funcion que se declara de forma flecha
-// propia de ES6
+//TODO: implement SOLID to organice files and code
 class WeatherLocation extends Component {
   constructor() {
     super();
@@ -46,17 +37,31 @@ class WeatherLocation extends Component {
     }
   };
 
+  getDataFromResponse = response => {
+    const { temp, humidity } = response.main;
+    const { main: iconWeather } = response.weather[0];
+    const { speed } = response.wind;
+    const dataReturn = {
+      temperature: temp.toFixed(1),
+      weatherState: weatherIcon[iconWeather.toUpperCase()],
+      humidity: humidity,
+      wind: `${speed} m/s`,
+    };
+    return dataReturn;
+  };
+ 
   handleUpdateClick = () => {
-    fetch(api_call);
-    console.log("actualizado");
-    this.setState({
-      /* cuando es llamado el método setea el estado lo que hace que
-      detecte que el estado ha cambiado y pase por render de nuevo */
-      city: 'Cadiz',
-      data: data2,
-      /* no haría falta pasar todos los campos que se van a actualizar.
-      con volver a pasar el objeto detectará los campos que se han cambiado. */
-    })
+    fetch(api_call).then(resolve => {
+      return resolve.json();
+    }).then(data => {
+      const weatherNow = this.getDataFromResponse(data);
+      this.setState({
+        city: 'Sevilla',
+        data: weatherNow,
+      })
+    }).catch(error => {
+      console.error(error)
+    });
   };
   render() {//primer render
     const { city, data } = this.state;
--- src/constans/weather.js
+++ src/constans/weather.js
@@ -1,9 +1,9 @@
-export const CLOUD  = "cloud";
+export const CLOUDS  = "cloud";
 export const CLOUDY = "cloudy";
 export const SUN    = "day-sunny";
 export const RAIN   = "rain";
 export const SNOW   = "snow";
 export const WINDY  = "windy";
 
-const weatherIcon = { CLOUD, CLOUDY, SUN, RAIN, SNOW, WINDY }
+const weatherIcon = { CLOUDS, CLOUDY, SUN, RAIN, SNOW, WINDY }
 export default weatherIcon;
