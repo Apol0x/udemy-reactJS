6e94b611698323c8d46bf96a3e695fab2062186f
apol0x <madejapolo@gmail.com>
2020-01-29 00:16:03 +0100

    MOD: receive name city from response

--- src/services/services.js
+++ src/services/services.js
@@ -6,6 +6,7 @@ const getDataFromResponse = response => {
     const { main: iconWeather } = response.weather[0];
     const { speed } = response.wind;
     const dataReturn = {
+      city: response.name,
       temperature: parseFloat(temp.toFixed(1)),
       weatherState: weatherIcon[iconWeather.toUpperCase()],
       humidity: humidity,
