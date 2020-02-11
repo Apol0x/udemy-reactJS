fd8ee1c8b59bcb0cb83f08e064ee71c58db49b0e
apol0x <madejapolo@gmail.com>
2020-02-11 20:11:51 +0100

    ADD: :memo: LocationList to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import WeatherLocation from './components/WeatherLocation/index';
+import LocationList from './components/LocationList.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <WeatherLocation city="Madrid, spa"/>
+        <LocationList />
+        {/* <WeatherLocation city="Madrid, spa"/> */}
       </div>
     );
   }
---
+++ src/components/LocationList.js
@@ -0,0 +1,10 @@
+import  React from "react";
+import WeatherLocation from '../components/WeatherLocation';
+
+const LocationList = () => (
+    <div>
+        <WeatherLocation city="Seville, spa"></WeatherLocation>
+    </div>
+);
+
+export default LocationList;
