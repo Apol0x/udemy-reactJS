02802cae91024dd2bb849dd26962eed19efac907
apol0x <madejapolo@gmail.com>
2020-02-18 01:32:59 +0100

    MOD: :pencil2: mapping cities to show WeatherLocation list

--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -1,10 +1,26 @@
-import  React from "react";
+import React from "react";
+import PropTypes from 'prop-types';
 import WeatherLocation from '../components/WeatherLocation';
 
-const LocationList = () => (
-    <div>
-        <WeatherLocation city="Seville, spa"></WeatherLocation>
-    </div>
-);
+const cityList = cities => {
+    return (
+        cities.map((cityMapped, index) =>
+            <WeatherLocation key={index} city={cityMapped} />
+        )
+    );
+};
+const LocationList = ({ cities }) => {
+
+    return (
+        <div>
+            {cityList(cities)}
+        </div>
+    );
+
+};
+
+LocationList.propTypes = {
+    cities: PropTypes.array.isRequired
+};
 
 export default LocationList;
