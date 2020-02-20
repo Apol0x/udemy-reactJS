7e919fd080125e6dd793001f514852a95e2e0750
apol0x <madejapolo@gmail.com>
2020-02-20 22:09:19 +0100

    MOD: :pencil2: using Grid-Row-Col by react-flexbox-grid

--- src/App.js
+++ src/App.js
@@ -1,31 +1,50 @@
 import React, { Component } from 'react';
-import './App.css';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
 import LocationList from './components/LocationList.js';
+import './App.css';
 
 const cities = [
   "Madrid, es",
   "Seville, es",
   "Berlin, ger"
 ];
-class App extends Component {
-  state = {
-    valueTest: "sin actualizar"
-  }
 
+class App extends Component {
+ 
   handlerClickWeatherLocation = (city) => {
     console.log("handlerClickWeatherLocation from App:", city);
-    this.setState({
-      valueTest: city
-    })
+   
   };
   render() {
     return (
-      <div className="App">
-        <LocationList cities={cities} onSelectWeatherLocation={this.handlerClickWeatherLocation} />
-        <div>Prueba de como sirve el "bubling" de evento {this.state.valueTest}</div>
-      </div>
+      <Grid>
+        <Row>
+          <AppBar position="sticky">
+            <Toolbar>
+              <Typography variant="title" color="inherit">
+                TITULO
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </Row>
+        <Row>
+          <Col xs={12} md={6}>
+            <LocationList
+              cities={cities}
+              onSelectWeatherLocation={this.handlerClickWeatherLocation}>
+            </LocationList>
+          </Col>
+          <Col xs={12} md={6}>
+            <div className="details"></div>
+          </Col>
+        </Row>
+      </Grid>
     );
   }
-}
+};
 
 export default App;
