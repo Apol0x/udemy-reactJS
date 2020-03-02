949254bbd264bb4c3b1d8570d70bd6096fcc7190
apol0x <madejapolo@gmail.com>
2020-03-02 22:06:25 +0100

    MOD: add new constant to forecast apicall

--- src/constans/api_url.js
+++ src/constans/api_url.js
@@ -1,4 +1,5 @@
 import CONSTANT from './config';
 
 export const api_key = CONSTANT.API_WEATHER_KEY;
-export const url_base_weather = CONSTANT.URL_BASE;
+export const url_base_weather = CONSTANT.URL_BASE_WEATHER;
+export const url_base_forecast = CONSTANT.URL_BASE_FORECAST;
