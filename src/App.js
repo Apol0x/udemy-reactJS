a1a35d5907e727bf9ec367d4bb69a6d6632c552a
apol0x <madejapolo@gmail.com>
2020-03-23 23:22:15 +0100

    MOD: :sparkle: first redux-action & dispatch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
+import { createStore } from 'redux';
 import Paper from '@material-ui/core/Paper';
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
@@ -14,16 +15,20 @@ const cities = [
   "Berlin, ger"
 ];
 
+const store = createStore(() => { }, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
 class App extends Component {
   constructor() {
     super();
-    this.state = { city: null}
+    this.state = { city: null }
   }
   handlerClickWeatherLocation = (city) => {
     console.log("handlerClickWeatherLocation from App:", city);
     this.setState({
       city
     });
+    const action = { type: 'setCity', value: city};
+    store.dispatch(action);
   };
   render() {
     const { city } = this.state;
