ea5e280529b815184a40f4316faf881d35fda05c
apol0x <madejapolo@gmail.com>
2020-03-26 02:31:31 +0100

    MOD: :sparkle: using connect & mapDispatchToProps

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import { PropTypes } from 'prop-types';
 import { Grid, Row, Col } from 'react-flexbox-grid';
-import { createStore } from 'redux';
 import Paper from '@material-ui/core/Paper';
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
@@ -10,15 +11,12 @@ import './App.css';
 import ForeCastExtended from './components/ForeCastExtend.js';
 import { actionSetCity } from './actions';
 
+
 const cities = [
   "Madrid, es",
   "Seville, es",
   "Berlin, ger"
 ];
-
-//Inicializamos la conexión con el redux-devtool
-const store = createStore(() => { }, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-
 
 
 class App extends Component {
@@ -31,7 +29,7 @@ class App extends Component {
     this.setState({
       city
     });
-    store.dispatch(actionSetCity(city)); //despachamos la accion
+    this.props.actionSetCity(city)
   };
   render() {
     const { city } = this.state;
@@ -68,4 +66,12 @@ class App extends Component {
   }
 };
 
-export default App;
+App.propTypes = {
+  actionSetCity: PropTypes.func.isRequired,
+}
+
+const mapDispatchToProps = dispatch =>({
+  actionSetCity: value => dispatch(actionSetCity(value))
+});
+
+export default connect(null, mapDispatchToProps)(App);
