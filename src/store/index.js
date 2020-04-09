8f9d3588fd0ed2f849d00907d5383d9661ef5af3
apol0x <madejapolo@gmail.com>
2020-04-09 19:39:09 +0200

    MOD: redux-thunk installed and refactored store index

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,11 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import { city } from '../reducers/city.js';
 const initialState = {
     city: null,
 };
-
-
+//composición potenciada  para unir el middleware con el redux_devtools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 //Inicializamos la conexión con el redux-devtool
-//y definimo el store
-export const store = createStore(city,initialState,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+//y exportamos la definición del store
+export const store = createStore(city, initialState, composeEnhancers(applyMiddleware(thunk)));
