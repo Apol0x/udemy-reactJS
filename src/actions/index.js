f9a002862e7d446e9669ddc6ad5d51b35d54d507
apol0x <madejapolo@gmail.com>
2020-07-01 21:16:36 +0200

    MOD: refactored index actions by reduxthunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,32 @@
 /**
  * Acciones que se van a ir controladon en el reducer
  */
+import axios from 'axios';
+import getForecastByCity from '../services/getForecastByCity';
+import utilService from '../services/services.js'
+
 export const ACTIONS_CONSTANT = {
     SELECT_CITY_CONSULT: "SELECT_CITY_CONSULT",
+    SET_FORECAST_DATA: "SET_FORECAST_DATA",
 };
 //generamos acciones
-export const actionSetCity = (payload) => ({ type: ACTIONS_CONSTANT.SELECT_CITY_CONSULT, payload }) 
+const actionSetCity = payload => ({ type: ACTIONS_CONSTANT.SELECT_CITY_CONSULT, payload });
+const setForeCastData = payload => ({ type: ACTIONS_CONSTANT.SET_FORECAST_DATA, payload});
+
+export const setSelectedCity = (payload) => {
+    return async dispatch => {
+        //activar en el estado un indicador de busqueda
+        dispatch(actionSetCity(payload));
+
+        try {
+            const resp = await axios.post(getForecastByCity(payload));
+            console.log("RESPONSE FORECAST BEFORE TRANSFORM: ", resp);
+            const forecastData = utilService.getDataForecast(resp.data);
+            //modificar estado con el resultado de la promise
+            dispatch(setForeCastData({ city: payload, forecastData }));
+        }
+        catch (error) {
+            console.error("[msg] error at or when: ", error);
+        }
+    }
+}
--- src/containers/LocationListContainer.js
+++ src/containers/LocationListContainer.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import { PropTypes } from 'prop-types';
 import { connect } from 'react-redux';
 import LocationList from '../components/LocationList';
-import { actionSetCity } from '../actions';
+import { setSelectedCity } from '../actions';
 
 class LocationListContainer extends Component {
     handlerClickWeatherLocation = (cities) => {
         console.log("handlerClickWeatherLocation from App:", cities);
-        this.props.actionSetCity(cities)
+        this.props.setCity(cities)
     };
     render() {
         return (
@@ -26,7 +26,7 @@ LocationListContainer.propTypes = {
 };
 
 const mapDispatchToProps = dispatch => ({
-    actionSetCity: value => dispatch(actionSetCity(value))
+    setCity: value => dispatch(setSelectedCity(value))
 });
 
 export default connect(null, mapDispatchToProps)(LocationListContainer);
