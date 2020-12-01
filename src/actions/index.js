4dd6264b5a855d721aeafb938537e11396d1ba9b
apoloev <null>
2020-12-01 20:10:35 +0100

    MOD: add new actions-get/set WEATHER_CITY

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,22 @@
 /**
  * Acciones que se van a ir controladon en el reducer
  */
-import axios from 'axios';
 import getForecastByCity from '../services/getForecastByCity';
 import utilService from '../services/services.js'
+import getWeatherByCity from '../services/getUrlWeatherByCity';
+import axios from 'axios';
 
 export const ACTIONS_CONSTANT = {
-    SELECT_CITY_CONSULT: "SELECT_CITY_CONSULT",
-    SET_FORECAST_DATA: "SET_FORECAST_DATA",
+    SELECT_CITY_CONSULT: 'SELECT_CITY_CONSULT',
+    SET_FORECAST_DATA: 'SET_FORECAST_DATA',
+    GET_WEATHER_CITY: 'GET_WEATHER_CITY',
+    SET_WEATHER_CITY: 'SET_WEATHER_CITY'
 };
 //generamos acciones
 const actionSetCity = payload => ({ type: ACTIONS_CONSTANT.SELECT_CITY_CONSULT, payload });
-const setForeCastData = payload => ({ type: ACTIONS_CONSTANT.SET_FORECAST_DATA, payload});
+const setForeCastData = payload => ({ type: ACTIONS_CONSTANT.SET_FORECAST_DATA, payload });
+const setWeatherCity = payload => ({ type: ACTIONS_CONSTANT.SET_WEATHER_CITY, payload });
+const getWeatherCity = payload => ({ type: ACTIONS_CONSTANT.GET_WEATHER_CITY, payload });
 
 export const setSelectedCity = (payload) => {
     return async dispatch => {
@@ -29,4 +34,20 @@ export const setSelectedCity = (payload) => {
             console.error("[msg] error at or when: ", error);
         }
     }
-}
+};
+
+export const setWeather = payload => {
+    let apiCall = null;
+    return dispatch => {
+        payload.forEach(city => {
+            apiCall = getWeatherByCity(city)
+            dispatch(getWeatherCity(city));
+            axios.post(apiCall).then((res) => {
+                const weather = utilService.getDataFromResponse(res.data);
+                dispatch(setWeatherCity({ city, weather }));
+            }).catch(error => {
+                console.error(error.status + "  msg: " + error.statusText);
+            });
+        })
+    }
+};
