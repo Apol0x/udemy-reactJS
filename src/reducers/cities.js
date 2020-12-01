ac34613f0173073994f72c7da85b3c396f68fecc
apoloev <null>
2020-12-01 19:58:32 +0100

    confiure new selectors and reducer

--- src/reducers/cities.js
+++ src/reducers/cities.js
@@ -1,5 +1,6 @@
 import { ACTIONS_CONSTANT } from '../actions/index.js';
 import { createSelector } from "reselect";
+import toPairs from 'lodash.topairs';
 /**
  * Aquí crearemos la función que recogerá los cambios de 
  * estados que se vayan lanzando en la acción para pasarselas
@@ -7,15 +8,35 @@ import { createSelector } from "reselect";
  * @param {Estado de la acción} state 
  * @param {Accion que se va a controlar} action 
  */
-const { SET_FORECAST_DATA } = ACTIONS_CONSTANT;
+const {
+    SET_FORECAST_DATA,
+    GET_WEATHER_CITY,
+    SET_WEATHER_CITY
+} = ACTIONS_CONSTANT;
 export const cities = (state = {}, action) => {
     switch (action.type) {
-        case SET_FORECAST_DATA:
+        case SET_FORECAST_DATA: {
             const { city, forecastData } = action.payload;
-            return { ...state, [city]:{ forecastData }};
+            return { ...state, [city]: { forecastData } };
+        };
+        case GET_WEATHER_CITY: {
+            const city = action.payload;
+            return { ...state, [city]: { weather: null } }
+        };
+        case SET_WEATHER_CITY: {
+            const { city, weather } = action.payload;
+            return { ...state, [city]: { weather } }
+        };
+
         default:
             return state;
     }
 };
-
-export const getForecastDataFromCities = createSelector((state, city) => state[city] && state[city].forecastData, forecastData => forecastData);
+const fromObjToArray = cities => (toPairs(cities).map(([key, value]) => ({ key, name: key, data: value.weather })));
+export const getForecastDataFromCities = createSelector(
+    (state, city) => state[city] && state[city].forecastData, forecastData => forecastData
+);
+export const getWeatherCities = createSelector(
+    //(state => fromObjToArray(state), cities => cities)
+    (state => [], cities => cities)
+);
