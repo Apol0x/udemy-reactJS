f98f3ac18953c8d7c48dde5d92f4103d64bcd5e2
apol0x <madejapolo@gmail.com>
2020-12-17 20:07:35 +0100

    MOD: push to use apikey

---
+++ src/constans/config.js
@@ -0,0 +1,8 @@
+//This file while have sensitive data will not upload to repository
+export const CONSTANT = {
+    API_WEATHER_KEY: "013c68cf019e1c1e08a7168268b652e7",
+    URL_BASE_WEATHER: "https://api.openweathermap.org/data/2.5/weather",
+    URL_BASE_FORECAST: "https://api.openweathermap.org/data/2.5/forecast"
+};
+
+export default CONSTANT;
